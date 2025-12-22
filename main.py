1ff4c97936ca69c2a9f94240b6d36e10bd9f4168
Alex <41452060+gmh4589@users.noreply.github.com>
2025-12-22 16:18:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def horse_run(x, y):
   global counter
   may_press = 0
 
-  if buns[x][y]['text'] == "    ":
+  if buns[x][y]['text'] == "":
     counter += 1
-    btns[x][y]['text'] = str(counter).rjust(3, ' ') 
+    btns[x][y]['text'] = str(counter)
 
     for i in range(10):
       for j in range(10):
-        if btns[i][j]['text'] != "    ":
+        if btns[i][j]['text'] != "":
           btns[i][j].config(state='disabled', bg="#404040") 
         else:
           btns[i][j].config(state='disabled', bg="#808080") 
@@ -29,7 +29,7 @@ def horse_run(x, y):
       cx,cy = cx + x, cy + y
 
       try:
-        if cx < 0 or cy < 0 or btns[cx][cy]['text'] != "    ":
+        if cx < 0 or cy < 0 or btns[cx][cy]['text'] != "":
           continue
       except IndexError:
         continue
@@ -38,12 +38,16 @@ def horse_run(x, y):
     may_press += 1
 
   if may_press == 0:
-    messagebox.showinfo('MESSAGE', f" Game over, you're score: {counter}") 
+    if counter == 100:
+      messagebox.showinfo('MESSAGE', f'You win: {100}') 
+    else:
+      messagebox.showinfo('MESSAGE', f" Game over, you're score: {counter}") 
 
 for i in range(10):
   s = []
   for j in range(10):
-    btn = tk.Button(root, text="    ", command=lambda x=i, y=j: horse_run(x, y)) 
+    btn = tk.Button(root, text="", width=4, height=2, 
+                    command=lambda x=i, y=j: horse_run(x, y)) 
     btn.grid(row=i, column=j) 
     s.append(btn) 
 
